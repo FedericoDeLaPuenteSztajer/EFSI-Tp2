7a93127edd7ceb391a3763a0ce2ea6a4ebe37f9a
FEDERICODELAPUENTESZTAJER <49319314@est.ort.edu.ar>
2026-03-18 09:05:58 -0300

    final bug fix

--- script.js
+++ script.js
@@ -1,43 +1,63 @@
-
-const input = document.getElementById("guessinput")
-const button = document.getElementById("guessBtn")
+const input = document.getElementById("guessInput")
+const guessButton = document.getElementById("guessBtn")
+const restartButton = document.getElementById("resetBtn")
 const message = document.getElementById("message")
 const attemptsText = document.getElementById("attempts")
 
 const MIN = 1
 const MAX = 100
+const RandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-let secretNumber = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN
+let secretNumber = RandomNum(MIN, MAX)
 let attempts = 0
+let game=true
 
-button.addEventListener("click", checkGuess())
+function CheckGuess() {
 
-function checkGuess() {
-
-  if(input.value){
-
+  if(!game){
+    Restart()
   }
 
-  if (input.value == "" || Int(input.value) < MIN || Int(input.value) > MAX) {
-    alert("Dato inalido")
+  let guess = input.value
+
+  if (String(guess) == "" || guess < MIN || guess > MAX) {
+    alert("Dato invalido")
     return;
   }
-
-  const guess = input.value
   
-  attempts = attempts + 1
-
+  attempts+=1;
   attemptsText.innerText = attempts
 
-  if (guess === secretNumber) {
+
+  if (guess == secretNumber) {
     message.innerText = "¡Ganaste!"
-  }
-
-  if (guess < secretNumber) {
+    game=false
+  }else if (guess < secretNumber) {
     message.innerText = "El número es mayor"
-  }
-
-  if (guess > secretNumber) {
+  }else if (guess > secretNumber) {
     message.innerText = "El número es menor"
   }
+
+  if(attempts>10 && game){
+    alert("Perdiste :c")
+    game=false
+  }
+
 }
+
+function Restart(){
+  secretNumber= RandomNum(MIN, MAX)
+  attempts=0;
+  attemptsText.innerText = attempts
+  game=true
+  
+  message.innerText = ""
+  input.value= ""
+}
+ 
+guessButton.addEventListener("click", CheckGuess)
+restartButton.addEventListener("click", Restart)
+
+document.addEventListener('keydown', (event) => {
+  if(event.key=="Enter"){CheckGuess()}
+})
